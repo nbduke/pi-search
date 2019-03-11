863461b64bd8e29a078261627b090c241eb2b7fe
Nathan <nbduke@outlook.com>
2019-03-10 22:05:05 -0700

    Wrote a simple encoding program.

---
+++ Digits/encode_base_26.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	FILE *input, *output;
+	char c;
+	int alphabetIndex;
+	
+	if (argc != 2)
+	{
+		printf("encode_base_26 <input_file>");
+		return -1;
+	}
+
+	input = fopen(argv[1], "r");
+	output = fopen("encoding.txt", "w");
+	while (!(feof(input) || ferror(input)))
+	{
+		fscanf(input, "%c", &c);
+		if ('0' <= c && c <= '9')
+		{
+			alphabetIndex = (c - '0');
+		}
+		else if ('a' <= c && c <= 'z')
+		{
+			alphabetIndex = (c - 'a') + 10;
+		}
+		else
+		{
+			continue;
+		}
+		fprintf(output, "%c", 'a' + alphabetIndex);
+	}
+
+	fclose(input);
+	fclose(output);
+	return 0;
+}
